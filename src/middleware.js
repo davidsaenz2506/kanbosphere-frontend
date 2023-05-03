cb867f96390cd8c2622e7171886e6d590e8b1763
Laurav2612 <127690299+Laurav2612@users.noreply.github.com>
2023-05-03 02:42:23 -0500

    Night commit

--- src/middleware.js
+++ src/middleware.js
@@ -5,22 +5,6 @@ export async function middleware(request) {
   const jwt = request.cookies.get("tumbleToken");
   const finalToken = jwt?.value;
 
-  if (request.nextUrl.pathname.includes("/portalUser")) {
-    if (jwt === undefined) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-
-    try {
-      const { payload } = await jwtVerify(
-        finalToken,
-        new TextEncoder().encode(process.env.NEXTAUTH_SECRET)
-      );
-
-      return NextResponse.next();
-    } catch (error) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-  }
 
   return NextResponse.next();
 }
